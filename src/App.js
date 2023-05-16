2ab1391268f4056f2348c24ff83ffb91249e0663
Javier Calizaya Melendrez <javixcm@gmail.com>
2023-05-16 12:41:10 -0400

    base

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+   <div className="App">
+      <header className="App-header">
+          <span>Test Code</span>
+      </header>
+   </div>
+  );
+}
+
+export default App;
